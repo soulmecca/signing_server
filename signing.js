af981d3edf31a62e20ee04f8ac3a12bfb7bafbee
Danny Jung <soulmecca@gmail.com>
2015-10-08 12:57:35 -0400

    back to pre-signing url mode

--- signing.js
+++ signing.js
@@ -23,57 +23,57 @@ var read = fs.readFileSync('signing.json', 'utf8');
 var parsed = JSON.parse(read);
 
 app.get('/sign_s3', function(req, res){
-  // aws.config.update({accessKeyId: parsed.AWS_ACCESS_KEY, secretAccessKey: parsed.AWS_SECRET_KEY});
-  // var s3 = new aws.S3();
-  // var s3_params = {
-  //   Bucket: parsed.S3_BUCKET,
-  //   Key: req.query.file_name,
-  //   Expires: 300,
-  //   ContentType: 'image/jpeg',
-  //   ACL: 'public-read'
-  // };
-
-  var fileName = 'danny/'+req.query.file_name
-  var expiration = new Date(new Date().getTime() + 1000 * 60 * 5).toISOString();
-
-   var policy =
-   { "expiration": expiration,
-     "conditions": [
-       {"bucket": parsed.S3_BUCKET},
-       {"key": fileName},
-       {"acl": 'public-read-write'},
-       ["starts-with", "$Content-Type", ""]
-      //  ["content-length-range", 0, 524288000]
-  ]};
-
-  policyBase64 = new Buffer(JSON.stringify(policy), 'utf8').toString('base64');
-  signature = crypto.createHmac('sha1', parsed.AWS_SECRET_KEY).update(policyBase64).digest('base64');
-  var return_data = {
-    bucket: parsed.S3_BUCKET,
-    awsKey: parsed.AWS_ACCESS_KEY,
-    policy: policyBase64,
-    signature: signature,
-    url: 'https://' +parsed.S3_BUCKET+'.s3.amazonaws.com/'+req.query.file_name
+  aws.config.update({accessKeyId: parsed.AWS_ACCESS_KEY, secretAccessKey: parsed.AWS_SECRET_KEY});
+  var s3 = new aws.S3();
+  var s3_params = {
+    Bucket: parsed.S3_BUCKET,
+    Key: req.query.file_name,
+    Expires: 300,
+    ContentType: 'image/jpeg',
+    ACL: 'public-read'
   };
 
-  res.write(JSON.stringify(return_data));
-  res.end();
-
-
-
+  var fileName = 'danny/'+req.query.file_name
+  // var expiration = new Date(new Date().getTime() + 1000 * 60 * 5).toISOString();
   //
-  // s3.getSignedUrl('putObject', s3_params, function(err, data){
-  //     if(err){
-  //       console.log(err);
-  //     }else{
-  //       var return_data = {
-  //         signed_request: data,
-  //         url: 'https://' +parsed.S3_BUCKET+'.s3.amazonaws.com/'+req.query.file_name
-  //       };
-  //       res.write(JSON.stringify(return_data));
-  //       res.end();
-  //     }
-  // });
+  //  var policy =
+  //  { "expiration": expiration,
+  //    "conditions": [
+  //      {"bucket": parsed.S3_BUCKET},
+  //      {"key": fileName},
+  //      {"acl": 'public-read-write'},
+  //      ["starts-with", "$Content-Type", ""]
+  //     //  ["content-length-range", 0, 524288000]
+  // ]};
+  //
+  // policyBase64 = new Buffer(JSON.stringify(policy), 'utf8').toString('base64');
+  // signature = crypto.createHmac('sha1', parsed.AWS_SECRET_KEY).update(policyBase64).digest('base64');
+  // var return_data = {
+  //   bucket: parsed.S3_BUCKET,
+  //   awsKey: parsed.AWS_ACCESS_KEY,
+  //   policy: policyBase64,
+  //   signature: signature,
+  //   url: 'https://' +parsed.S3_BUCKET+'.s3.amazonaws.com/'+req.query.file_name
+  // };
+  //
+  // res.write(JSON.stringify(return_data));
+  // res.end();
+
+
+
+
+  s3.getSignedUrl('putObject', s3_params, function(err, data){
+      if(err){
+        console.log(err);
+      }else{
+        var return_data = {
+          signed_request: data,
+          url: 'https://' +parsed.S3_BUCKET+'.s3.amazonaws.com/'+req.query.file_name
+        };
+        res.write(JSON.stringify(return_data));
+        res.end();
+      }
+  });
 });
 
 
